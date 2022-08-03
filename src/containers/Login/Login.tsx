73b987fc8dfbe9e365cb1c8ee0f9ae02b97d86f5
Innush <innasol90@gmail.com>
2022-08-03 11:51:38 -0400

    adding StyleSheet and text

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,20 +1,1 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-  },
-});
-
-const Login = () => {
-  return (
-    <View style={styles.container}>
-      {/*<Typography>*/}
-      {/*    {'Hello, we are looking for pets'}*/}
-      {/*</Typography>*/}
-    </View>
-  );
-};
-
-export default Login
+import React from 'react';import {StyleSheet, Text, View} from 'react-native';const styles = StyleSheet.create({  container: {    marginTop: 50,  },});const Login = () => {  return (    <View style={styles.container}>      <Text>{'Hello, we are looking for pets'}</Text>    </View>  );};export default Login
