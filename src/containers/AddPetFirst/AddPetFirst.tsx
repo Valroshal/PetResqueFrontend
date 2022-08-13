5d748408aaedcda532c6ad88a9a403b555678cde
Valroshal <valerie.kabishcher@gmail.com>
2022-08-13 11:15:06 +0300

    first page found/lost

--- src/containers/AddPetFirst/AddPetFirst.tsx
+++ src/containers/AddPetFirst/AddPetFirst.tsx
@@ -5,21 +5,21 @@ import AddRemoveCard from "../components/AdddRemoveCard/AddRemoveCard";
 
 const AddPetFirst = () => {
   return(
-      <View>
-          <TopLogo />
-          <View style={{marginTop:22 , padding: 30}}>
-              <View style={{marginBottom: 40}}>
-                <AddRemoveCard
-                    upperText="Add lost pet"
-                    lowerText="I`ve lost my pet and want to find it"
-                />
-              </View>
-              <View>
-                  <AddRemoveCard
-                      upperText="Add found pet"
-                      lowerText="I`ve found a pet and looking for it`s owners"
-                  />
-              </View>
+      <View style={{paddingHorizontal: 30, paddingTop: 46}}>
+          <View style={{marginBottom: 52}}>
+              <TopLogo/>
+          </View>
+          <View style={{marginBottom: 40}}>
+              <AddRemoveCard
+                  upperText="Add lost pet"
+                  lowerText="I`ve lost my pet and want to find it"
+              />
+          </View>
+          <View>
+              <AddRemoveCard
+                  upperText="Add found pet"
+                  lowerText="I`ve found a pet and looking for it`s owners"
+              />
           </View>
       </View>
   )
