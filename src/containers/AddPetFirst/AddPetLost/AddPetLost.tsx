48ac40fc91d920384738794dfd60d8a46b59e749
Innush <innasol90@gmail.com>
2022-08-21 18:01:15 -0400

    create containers for better view

--- src/containers/AddPetFirst/AddPetLost/AddPetLost.tsx
+++ src/containers/AddPetFirst/AddPetLost/AddPetLost.tsx
@@ -1,27 +1,41 @@
 import * as React from 'react';
 import {
+  Alert,
+  Image,
   StyleSheet,
-  Text,
+  Text, TextInput, TextStyle,
   TouchableOpacity,
   View,
   ViewStyle,
 } from 'react-native';
-import ImagePicker from 'react-native-image-picker';
+import {useState} from "react";
+const ImagePicker = require('react-native-image-picker');
 
 const styles = StyleSheet.create({
-  title: {
+  sub_title_text: {
+    fontSize: 16,
+    fontFamily: 'Lato',
+  } as TextStyle,
+  title_text: {
+    fontSize: 20,
+    fontFamily: 'Lato',
+  } as TextStyle,
+  container: {
     justifyContent: 'flex-start',
     paddingHorizontal: 20,
+    marginBottom: 40,
+  } as ViewStyle,
+  title: {
+    justifyContent: 'flex-start',
     paddingBottom: 12,
   } as ViewStyle,
   smallText: {
     justifyContent: 'flex-start',
     paddingLeft: 20,
-    paddingRight: 50,
+    paddingRight: 70,
     paddingBottom: 15,
   } as ViewStyle,
   btn: {
-    marginHorizontal: 20,
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#DDDDDD',
@@ -31,8 +45,8 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     borderWidth: 1,
     borderColor: '#28230E',
+    width: 157 ,
   } as ViewStyle,
-  imageStyle: {},
 });
 
 interface Props {
@@ -40,66 +54,94 @@ interface Props {
 }
 
 const AddPetLost: React.FC<Props> = ({navigation}) => {
+  const [photos , setPhotos] = useState([])
+
   const addPetType = () => {
     navigation.navigate('AddPetFirst');
   };
 
   const handleChoosePhoto = () => {
     let options = {
-      maxWidth: 300,
-      maxHeight: 550,
-      quality: 1,
+      selectionLimit: 0
     };
-    ImagePicker.launchImageLibrary(options, response => {
+    ImagePicker.launchImageLibrary(options, (response: any) => {
       console.log('response', response);
+      if (response.didCancel) {
+        console.log('User cancelled photo picker');
+        Alert.alert('You did not select any image');
+      }
+      else {
+        setPhotos(response.assets);
+        console.log("set to photo: ", response);
+        console.log("photos" , photos);
+      }
     });
   };
+
+
 
   return (
     <View>
       <View style={styles.smallText}>
-        <Text style={{fontSize: 16, fontFamily: 'Lato', color: '#28230E'}}>
+        <Text style={[styles.sub_title_text, {color: '#28230E'}]}>
           Here are details of your dog - ֿMars. You can edit it here
         </Text>
       </View>
-      <View style={styles.title}>
-        <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#000'}}>
-          Animal Type
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.title}>
+          <Text style={[styles.title_text , { color: '#000'}]}>
+            Animal Type
+          </Text>
+        </View>
+          <TouchableOpacity style={styles.btn} onPress={addPetType}>
+            <Text style={[styles.sub_title_text ,{padding: 13.5, color: '#6C6C6C'}]}>
+              Please choose type
+            </Text>
+          </TouchableOpacity>
       </View>
-      <TouchableOpacity style={styles.btn} onPress={addPetType}>
-        <Text
-          style={{
-            padding: 13.5,
-            fontSize: 16,
-            fontFamily: 'Lato',
-            color: '#6C6C6C',
-          }}>
-          Please choose type
-        </Text>
-      </TouchableOpacity>
-      <View style={styles.title}>
-        <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#000'}}>
-          Add Photos
-        </Text>
-        <TouchableOpacity style={styles.btnPhoto} onPress={handleChoosePhoto}>
-          {/*<Image style={styles.imageStyle}*/}
-          {/*       source={require('/src/assets/images/camera.png')} />*/}
-          <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#28230E'}}>
+      <View style={styles.container}>
+        <View style={styles.title}>
+          <Text style={[styles.title_text,{ color: '#000'}]}>
             Add Photos
           </Text>
-        </TouchableOpacity>
+        </View>
+          {photos && photos.map( (item) => {
+            return (
+                <View>
+                  <Image
+                      source={{uri: item['uri']}}
+                      style={{width: 96, height: 96}}
+                  />
+                </View>
+              )
+            })
+          }
+          <TouchableOpacity style={styles.btnPhoto} onPress={handleChoosePhoto}>
+            {/*<Image style={styles.imageStyle}*/}
+            {/*       source={require('/src/assets/images/camera.png')} />*/}
+            <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#28230E'}}>
+              Add Photos
+            </Text>
+          </TouchableOpacity>
       </View>
-
-      <View style={styles.title}>
-        <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#000'}}>
-          Description
-        </Text>
-      </View>
-      <View>
-        <Text style={{fontSize: 16, fontFamily: 'Lato', color: '#595959'}}>
-          Here you can describe color, size, breed of the pet
-        </Text>
+      <View style={styles.container} >
+        <View style={styles.title}>
+          <Text style={[styles.title_text , {color: '#000'}]}>
+            Description
+          </Text>
+        </View>
+        <View style={styles.smallText}>
+          <Text style={[styles.sub_title_text, {color: '#595959'}]}>
+            Here you can describe color, size, breed of the pet
+          </Text>
+        </View>
+        <View style={styles.btn}>
+          <TextInput placeholderTextColor='#28230E'
+                     placeholder="Shy and fluffy, retriever"
+                     style={ {padding: 10, paddingBottom: 150, fontSize: 16}}
+          >
+          </TextInput>
+        </View>
       </View>
     </View>
   );
