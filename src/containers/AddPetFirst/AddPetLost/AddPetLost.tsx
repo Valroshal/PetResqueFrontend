b7b336b7fdb9b5a492118598434e67806ef02f0d
Innush <innasol90@gmail.com>
2022-08-30 10:48:37 -0400

    fix few images in one-time

--- src/containers/AddPetFirst/AddPetLost/AddPetLost.tsx
+++ src/containers/AddPetFirst/AddPetLost/AddPetLost.tsx
@@ -54,6 +54,13 @@ const styles = StyleSheet.create({
     borderColor: '#28230E',
     width: 157 ,
   } as ViewStyle,
+  photoGrid: {
+    display: 'flex',
+    maxWidth: 350,
+    flexWrap: 'wrap',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+  } as ViewStyle,
 });
 
 interface Props {
@@ -67,99 +74,108 @@ const AddPetLost: React.FC<Props> = ({navigation}) => {
     navigation.navigate('AddPetFirst');
   };
 
+  const onContentSizeChange = () => {
+
+  }
+
   //comment
   const handleChoosePhoto = () => {
     let options = {
       selectionLimit: 0,
     };
+    console.log("In function")
     ImagePicker.launchImageLibrary(options, (response: any) => {
       if (response.didCancel) {
         Alert.alert('You did not select any image');
       }
       else {
-        setPhotos((photos) => [...photos, response.assets]);
-        console.log("photos" , photos)
-        console.log("response.assets" , )
+        setPhotos((photos) => [...photos,...response.assets]);
       }
     });
   };
 
   return (
-      <ScrollView>
-        <View style={{padding: 20}}>
-            <View style={styles.smallText}>
-              <Text style={[styles.subTitleText, {color: '#28230E'}]}>
-                Here are details of your dog - ֿMars. You can edit it here
+    <ScrollView
+        style={{flex:1}}
+        horizontal={true}
+        onContentSizeChange={onContentSizeChange}
+    >
+      <View style={{padding: 20}}>
+          <View style={styles.smallText}>
+            <Text style={[styles.subTitleText, {color: '#28230E'}]}>
+              Here are details of your dog - ֿMars. You can edit it here
+            </Text>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.title}>
+              <Text style={[styles.titleText , { color: '#000'}]}>
+                Animal Type
               </Text>
             </View>
-            <View style={styles.container}>
-              <View style={styles.title}>
-                <Text style={[styles.titleText , { color: '#000'}]}>
-                  Animal Type
+              <TouchableOpacity style={styles.btn} onPress={addPetType}>
+                <Text style={[styles.subTitleText ,{padding: 13.5, color: '#6C6C6C'}]}>
+                  Please choose type
                 </Text>
-              </View>
-                <TouchableOpacity style={styles.btn} onPress={addPetType}>
-                  <Text style={[styles.subTitleText ,{padding: 13.5, color: '#6C6C6C'}]}>
-                    Please choose type
-                  </Text>
-                </TouchableOpacity>
+              </TouchableOpacity>
+          </View>
+          <View style={styles.container}>
+            <View style={styles.title}>
+              <Text style={[styles.titleText,{ color: '#000'}]}>
+                Add Photos
+              </Text>
             </View>
-            <View style={styles.container}>
-              <View style={styles.title}>
-                <Text style={[styles.titleText,{ color: '#000'}]}>
+            <View style={styles.photoGrid}>
+              {photos && photos.map( (item,index) => {
+                return (
+                  <View key={index}>
+                        <Image
+                            source={{uri: item.uri}}
+                            style={{width: 96, height: 96, marginBottom: 12 , marginRight: 12 }}
+                        />
+
+                  </View>
+                )
+                })}
+            </View>
+              <TouchableOpacity
+                  style={styles.btnPhoto}
+                  onPress={handleChoosePhoto}
+              >
+                <Image
+                    source={require('/Users/innagro/PetResqueFrontend/src/assets/images/camera.png')}
+                />
+                <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#28230E'}}>
                   Add Photos
                 </Text>
-              </View>
-                {photos && photos.map( (item) => {
-                  return (
-                      <View>
-                        <Image
-                            source={{uri: item['uri']}}
-                            style={{width: 96, height: 96, marginBottom: 12}}
-                        />
-                      </View>
-                    )
-                  })
-                }
-                <TouchableOpacity
-                    style={styles.btnPhoto}
-                    onPress={handleChoosePhoto}
-                >
-                  <Image
-                      source={require('/Users/innagro/PetResqueFrontend/src/assets/images/camera.png')}
-                  />
-                  <Text style={{fontSize: 20, fontFamily: 'Lato', color: '#28230E'}}>
-                    Add Photos
-                  </Text>
-                </TouchableOpacity>
+              </TouchableOpacity>
+          </View>
+          <View style={styles.container} >
+            <View style={styles.title}>
+              <Text style={[styles.titleText , {color: '#000'}]}>
+                Description
+              </Text>
             </View>
-            <View style={styles.container} >
-              <View style={styles.title}>
-                <Text style={[styles.titleText , {color: '#000'}]}>
-                  Description
-                </Text>
-              </View>
-              <View style={styles.smallText}>
-                <Text style={[styles.subTitleText, {color: '#595959'}]}>
-                  Here you can describe color, size, breed of the pet
-                </Text>
-              </View>
-              <View style={styles.btn}>
-                <TextInput
-                    multiline={true}
-                    numberOfLines={4}
-                    placeholderTextColor='#28230E'
-                    placeholder="Shy and fluffy, retriever"
-                    style={{
-                      padding: 10,
-                      fontSize: 16
-                    }}
-                >
-                </TextInput>
-              </View>
+            <View style={styles.smallText}>
+              <Text style={[styles.subTitleText, {color: '#595959'}]}>
+                Here you can describe color, size, breed of the pet
+              </Text>
             </View>
-        </View>
-      </ScrollView>
+            <View style={styles.btn}>
+              <TextInput
+                  multiline={true}
+                  numberOfLines={4}
+                  placeholderTextColor='#28230E'
+                  placeholder="Shy and fluffy, retriever"
+                  style={{
+                    padding: 10,
+                    fontSize: 16
+                  }}
+              >
+              </TextInput>
+            </View>
+          </View>
+      </View>
+    </ScrollView>
   );
 };
 
