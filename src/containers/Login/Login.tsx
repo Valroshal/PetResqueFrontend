9135eda88603ac62addac77eaf2e5fa6ffb7b34f
Innush <innasol90@gmail.com>
2022-09-05 22:29:35 -0400

    part1 login

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,16 +1,46 @@
 import * as React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextStyle, View, ViewStyle} from 'react-native';
+import TopLogo from "../components/TopLogo/TopLogo";
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 50,
+    marginTop: 46,
   },
+  subContainer: {
+    padding : 20,
+    paddingTop: 71,
+  } as ViewStyle,
+  header: {
+      fontSize: 32,
+      fontFamily: 'Lato',
+      color: "#28230E",
+      alignItems: "center",
+  }  as TextStyle,
+  subHeader: {
+      fontSize: 16,
+      fontStyle: "normal",
+      color: "#28230E",
+      fontFamily: 'Lato',
+  }
+
 });
 
 const Login = () => {
   return (
     <View style={styles.container}>
-      <Text>{'Hello, we are looking for pets'}</Text>
+      <TopLogo />
+      <View style={styles.subContainer}>
+        <Text style={[styles.header , {paddingBottom: 15}]}>
+            {'Log in'}
+        </Text>
+        <Text style={styles.subHeader}>
+            {'Log in to search for your pet or connect to \n' +
+            'other pet lovers and help them to find and\n' +
+            'return pets to their home'}
+        </Text>
+
+      </View>
+
     </View>
   );
 };
