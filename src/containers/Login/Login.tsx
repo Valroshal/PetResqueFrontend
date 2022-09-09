618adaddb7c33e72da2502a93cc00cd5f885fc7f
Innush <innasol90@gmail.com>
2022-09-08 21:10:19 -0400

    add error style

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as yup from 'yup';
-import {StyleSheet, Text, TextInput, TextStyle, View, ViewStyle} from 'react-native';
-import {Field, Formik} from 'formik';
+import {Image, StyleSheet, Text, TextInput, TextStyle, View, ViewStyle} from 'react-native';
+import {Formik} from 'formik';
 import TopLogo from "../components/TopLogo/TopLogo";
 import GlobalButton from "../components/GlobalButton/GlobalButton";
+import {Ex} from "../../assets/images/Ex.png";
 
 const styles = StyleSheet.create({
   container: {
@@ -40,6 +41,18 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.15,
     elevation: 1,
   } as ViewStyle,
+  fieldError: {
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#EC6868',
+    marginBottom: 20,
+    shadowColor: '#000',
+    backgroundColor: "#FFE8E8" ,
+    shadowOffset: {width: 6, height: 6},
+    shadowRadius: 10,
+    shadowOpacity: 0.15,
+    elevation: 1,
+  } as ViewStyle,
   inputText: {
     padding: 10,
     paddingVertical:13.5,
@@ -47,6 +60,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily:'Lato' ,
     color: '#6C6C6C',
+  } as TextStyle,
+  inputTextError: {
+    padding: 10,
+    paddingTop: 6,
+    paddingBottom: 26,
+    alignItems: "flex-start",
+    fontSize: 12,
+    fontFamily:'Lato' ,
   } as TextStyle,
   errorText: {
     fontSize: 10,
@@ -99,23 +120,33 @@ const Login = () => {
               // isValid,
           }) => (
             <>
-              <View style={styles.field}>
+              <View
+                style={(errors.email && touched.email) ? styles.fieldError : styles.field}
+              >
                   <TextInput
                     placeholder="Email"
-                    style={styles.inputText}
+                    style={(errors.email && touched.email) ? [styles.inputTextError,{color: '#EC6868'}] :styles.inputText}
                     onChangeText={handleChange('email')}
                     onBlur={handleBlur('email')}
                     value={values.email}
                     keyboardType="email-address"
                   />
+                {(errors.email && touched.email) &&
+                  <Image
+                    // style={}
+                    source={Ex}
+                  />
+                }
               </View>
               {(errors.email && touched.email) &&
                 <Text style={styles.errorText}>{errors.email}</Text>
               }
-              <View style={styles.field}>
+              <View
+                style={(errors.password && touched.password) ? styles.fieldError :styles.field}
+              >
                   <TextInput
                     placeholder="Password"
-                    style={styles.inputText}
+                    style={(errors.password && touched.password) ? [styles.inputTextError,{color: '#EC6868'}] : styles.inputText}
                     onChangeText={handleChange('password')}
                     onBlur={handleBlur('password')}
                     value={values.password}
