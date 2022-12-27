415e6779060187f0c8a979cef275adffd81e7d41
Innush <innasol90@gmail.com>
2022-12-27 10:33:53 -0600

    started OnBoarding

---
+++ src/containers/OnBoarding/OnBoarding.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import TopLogo from '../components/TopLogo/TopLogo';
+import {Text, View} from "react-native";
+
+
+
+const OnBoarding = () => {
+
+    return(
+        <View>
+            <TopLogo />
+
+            <View>
+                <Text>
+
+                </Text>
+            </View>
+        </View>
+    )
+
+
+
+}
+
+export default OnBoarding;
